db0020ae13452fff8c23116ba095353459231bc4
MikeB1124 <56797038+MikeB1124@users.noreply.github.com>
2022-11-19 11:51:37 -0800

    added fetch function

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const print = require("./print")
 
+
+function get_new_orders(){
+    fetch('https://www.wixapis.com/restaurants/v3/orders',{
+        headers: {
+            "Authorization": "xxx",
+            'Content-Type': 'application/json',
+            "wix-account-id": "xxx",
+            "wix-site-id": "xxx"
+        },
+        body: JSON.stringify({"status": "NEW"})
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
+
 // exports.handler = async(event) => {
 //     console.log(event)
 //     print("if this works we good")
